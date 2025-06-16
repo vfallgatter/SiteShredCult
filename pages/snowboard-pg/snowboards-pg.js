c134354a6498b96210d33ae8ddbf1f5a082b49d9
vfallgatter <vitorhugofallgatter@gmail.com>
2025-06-16 17:21:50 -0300

    SnowBoard Page Started-JavaScrimt implemented

--- pages/snowboard-pg/snowboards-pg.js
+++ pages/snowboard-pg/snowboards-pg.js
@@ -19,4 +19,30 @@
     // Troca automática a cada 3 segundos
     setInterval(() => {
       avancar();
-    }, 10000);
+    }, 6000);
+
+
+  filterSelection("all"); // Exibir todos no início
+
+  function filterSelection(c) {
+    const x = document.getElementsByClassName("filterDiv");
+    if (c === "all") c = "";
+    for (let i = 0; i < x.length; i++) {
+      x[i].classList.remove("show");
+      if (x[i].className.indexOf(c) > -1) {
+        x[i].classList.add("show");
+      }
+    }
+  }
+
+  const btnContainer = document.getElementById("button_container");
+  const btns = btnContainer.getElementsByClassName("btn");
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      const current = document.getElementsByClassName("active");
+      if (current.length > 0) {
+        current[0].classList.remove("active");
+      }
+      this.classList.add("active");
+    });
+  }
